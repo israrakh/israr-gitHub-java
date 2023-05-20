cce90a11737b03aada743e4ae84598c1f9e5b6c1
Israr <israr.akh03@gmail.com>
2023-05-19 20:54:04 -0400

    Added ForEach Loops Class

---
+++ src/github/java/ForEachLoopsPractice.java
@@ -0,0 +1,12 @@
+package github.java;
+
+public class ForEachLoopsPractice {
+
+	public static void main(String[] args) {
+
+		String[] cities = { "Japan", "Afghanistan", "Germany", "Canada", "India", "Maldieve" };
+		for (String city : cities) {
+			System.out.println(city);
+		}
+	}
+}
